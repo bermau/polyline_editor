ca5b5f48a75975ecebd3b22765ac74df4c7c1bb9
bertrand <bertrand.maubert.info@gmail.com>
2023-12-11 11:19:53 +0100

    corrections of the position of the pen (up or down) according the different paths in the letter.

--- essais/example_for_data_of_digits.py
+++ essais/example_for_data_of_digits.py
@@ -1,5 +1,6 @@
 SIZE_X = 150
 SIZE_Y = 200
+__version__ = "0.1"
 # Data are plotted from my polyline_editor tool.
 data = {
     '0': [[(80, 9), (49, 13), (27, 35), (14, 60), (11, 98), (11, 137), (18, 168), (51, 185), (72, 191), (83, 190),
@@ -7,9 +8,9 @@ data = {
     '1': [[(23, 85), (86, 13), (85, 190)]],
     '2': [[(31, 64), (42, 41), (64, 26), (83, 23), (102, 36), (116, 56), (118, 90), (106, 108), (82, 134), (60, 148),
            (28, 176), (28, 183), (117, 182)]],
-    '3': [[(8, 35), (48, 20), (72, 23), (92, 28), (106, 46), (115, 69), (113, 90), (94, 110), (87, 182), (45, 112)],
+    '3': [[(8, 35), (48, 20), (72, 23), (92, 28), (106, 46), (115, 69), (113, 90), (94, 110), (45, 112)],
           [(45, 115), (109, 122), (112, 140), (113, 158), (106, 173), (70, 187), (34, 186), (10, 172)]],
-    '4': [[(64, 12), (5, 102), (94, 101)], [], [(64, 187)], [(67, 6), (69, 193)]],
+    '4': [[(64, 12), (5, 102), (94, 101)],  [(64, 187)], [(67, 6), (69, 193)]],
     '5': [
         [(11, 18), (13, 99), (56, 93), (82, 100), (107, 118), (112, 138), (111, 171), (70, 191), (28, 189), (12, 179)],
         [(11, 12), (107, 16)]],
@@ -46,8 +47,8 @@ for key in data:
 
 
 # Maintenant les données sont en mode cartésien, ce qui me semble plus simple à utiliser.
-PEN_UP = "G0 Z1"
-PEN_DOWN = "G0 Z-1"
+PEN_UP = "G0 Z1 ; pen up"
+PEN_DOWN = "G0 Z-1 ; pen down"
 
 class Tracer:
     def __init__(self):
@@ -68,25 +69,42 @@ class Tracer:
             raise ValueError(f"Error for letter '{lettre}'")
         else:
             gcode = [f"; letter {letter}"]
+
             for i, path in enumerate(data_cart[lettre]):
                 gcode.append(f"; path {i} for letter {lettre}")
+                # first point : move then down the pen
+                point = path[0]
+                x, y = self.correct_x(point), self.correct_y(point)
+                gcode.append(f"G0 X{str(x)} Y{str(y)}")
                 gcode.append(PEN_DOWN)
-                for point in path:
-                    x = point[0] / SIZE_X * self.font_with + self.cur_x
-                    y = point[1] / SIZE_Y * self.font_height + self.cur_y
-                    gcode.append(f"G0 X{str(x)} Y{str(y)}")
-                gcode.append(PEN_UP)
-            self.cur_x +=  2  # mm
+                if len(path) > 1:
+                    for point in path[1:]:
+                        x, y = self.correct_x(point), self.correct_y(point)
+                        gcode.append(f"G0 X{str(x)} Y{str(y)}")
+                    gcode.append(PEN_UP)
+            self.cur_x += 2  # mm
             return gcode
+
+    def correct_y(self, point) -> float:
+        y = point[1] / SIZE_Y * self.font_height + self.cur_y
+        return y
+
+    def correct_x(self, point):
+        x = point[0] / SIZE_X * self.font_with + self.cur_x
+        return x
 
 
 if __name__ == '__main__':
     t = Tracer()
-    word = "23345643"
+    word = "234"
     retour_x, retour_y = (0, 0)
-    code = []
+    code = [f"; version {__version__}"]
+    code.append(PEN_UP)
     for letter in word:
         code += t.tracer_lettre(letter)
     for line in code:
         print(line)
 
+    with open("./sortie.gc", "w") as f:
+        for line in code:
+            f.write(line+"\n")
